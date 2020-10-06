b0c5ae49f336bcd27614df4709bce4f51452fab7
vs4711 <vijendersharma47@gmail.com>
2020-10-06 19:48:11 +0530

    Updates

---
+++ precache-manifest.51e783ca29e3c62a6b8a5ba8315b64a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40fb4c43ab6a12c4b4204b9291198eed",
+    "url": "/archex/index.html"
+  },
+  {
+    "revision": "ce58c6fdab68cb779691",
+    "url": "/archex/static/js/2.1c1cead0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/archex/static/js/2.1c1cead0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc055228f9ca03ecd54a",
+    "url": "/archex/static/js/main.c3668863.chunk.js"
+  },
+  {
+    "revision": "475ef42ad2bea5383db5",
+    "url": "/archex/static/js/runtime-main.d6403276.js"
+  }
+]);
